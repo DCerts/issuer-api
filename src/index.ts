e3cceb37b7ddae4755e5082eb9c62757f853d938
vi <tuanm.dev@gmail.com>
2022-01-17 09:08:18 +0700

    fix typo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express } from "express";
+import express, { Express } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { createAllTables, connect, createSchoolAccounts } from './utils/db';
